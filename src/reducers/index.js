42548aa15ed799b1a7a80f6c294623c23a944fae
gabrielchase <gchase.patron@gmail.com>
2018-01-14 23:30:53 +0800

    Add reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,14 @@
 import { combineReducers } from 'redux'
 import { reducer as formReducer } from 'redux-form'
 
+import UserReducer from './reducer_users'
+import AuthReducer from './reducer_auth'
+
+
 const rootReducer = combineReducers({
-    form: formReducer
+    form: formReducer,
+    auth: AuthReducer,
+    user: UserReducer,
 })
 
 export default rootReducer
