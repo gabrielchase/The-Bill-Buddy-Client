89d75c4d710f5b2818cafba839e944b87274498b
gabrielchase <gchase.patron@gmail.com>
2018-01-14 23:31:23 +0800

    Create `fetchUser` action and add to `componentDidMount` of Dashboard component

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,21 @@
 import axios from 'axios'
 import { LOCAL_ROOT_URL } from '../const'
 
-export const CREATE_USER = 'create_user'
 export const LOGIN_USER_SUCCESS = 'login_user_success'
 export const LOGIN_USER_FAILURE = 'login_user_failure'
+
+export const CREATE_USER = 'create_user'
+export const FETCH_USER = 'fetch_user'
+
+
+export function getHeaders(jwt) {
+    return {
+        headers: {
+            'Authorization': `JWT ${jwt}`,
+            'Content-Type' : 'application/json'
+        }
+    }
+}
 
 
 export async function createUser(values) {
@@ -14,9 +26,21 @@ export async function createUser(values) {
     }
 }
 
+export async function fetchUser() {
+    let user_id = localStorage.getItem('user_id')
+    let jwt = localStorage.getItem('jwt')
+    let headers = getHeaders(jwt)
+    const res = await axios.get(`${LOCAL_ROOT_URL}/users/${user_id}/`, headers)
+    return {
+        type: FETCH_USER, 
+        payload: res.data
+    }
+}
+
 export async function loginUser(values) {
     const res = await axios.post(`${LOCAL_ROOT_URL}/login/`, values)
-    console.log(res.data)
+    await localStorage.setItem('user_id', res.data.user_id)
+    await localStorage.setItem('jwt', res.data.token)
     return {
         type: LOGIN_USER_SUCCESS, 
         payload: res.data
--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -2,15 +2,29 @@ import React, { Component } from 'react'
 import { Field, reduxForm } from 'redux-form'
 import { connect } from 'react-redux'
 
+import { fetchUser } from '../actions'
+
 
 class Dashboard extends Component {
+    async componentDidMount() {
+        await this.props.fetchUser()
+    }
+
     render() {
+        const { user } = this.props
         return (
             <div>
                 <h1>Dashboard</h1>
+                <p>Welcome {user.first_name} {user.last_name}</p>
             </div>
         )
     }
 }
 
-export default Dashboard
+function mapStateToProps(state) {
+    return { 
+        user: state.user
+    }
+}
+
+export default connect(mapStateToProps, { fetchUser })(Dashboard)
