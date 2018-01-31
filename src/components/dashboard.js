085c7c716726e4830a6a486cd0a6d4c1480c999b
gabrielchase <gchase.patron@gmail.com>
2018-01-31 23:29:28 +0800

    Create piechart

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import {PieChart, Pie, Legend, Tooltip } from 'recharts'
 
 import { fetchUser, fetchBills } from '../actions'
 
 import _ from 'lodash'
+
+
+const data01 = [{name: 'Group A', value: 400}, {name: 'Group B', value: 300},
+                  {name: 'Group C', value: 300}, {name: 'Group D', value: 200},
+                  {name: 'Group E', value: 278}, {name: 'Group F', value: 189}]
 
 
 class Dashboard extends Component {
@@ -13,7 +19,6 @@ class Dashboard extends Component {
 
     renderPaymentsThisMonth() {
         let { user: { payments_this_month } } = this.props
-        console.log(payments_this_month)
         return (
             _.map(payments_this_month, (payment, idx) => {
                 return (
@@ -58,6 +63,10 @@ class Dashboard extends Component {
                     </div>
                     <div className="column" id="expenditure">
                         <h4 className="center-text">Expenditure This Year</h4>
+                        <PieChart width={800} height={300} className="pie-chart">
+                            <Pie data={data01}  cx="50%" cy="50%" fill="#8884d8" label/>
+                            <Tooltip/>
+                        </PieChart>
                     </div>
                 </div>
                 
