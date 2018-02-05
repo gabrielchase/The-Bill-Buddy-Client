0fedc77f254d8669f4693be8dc135358699feb64
gabrielchase <gchase.patron@gmail.com>
2018-02-05 12:24:57 +0800

    Move multiple payments

--- src/components/bill_new.js
+++ src/components/bill_new.js
@@ -3,21 +3,11 @@ import { Field, reduxForm } from 'redux-form'
 import { connect } from 'react-redux'
 
 import { createBill } from '../actions'
-import CheckboxGroup  from './checkbox_group'
-
-import { MONTHS, DAYS } from '../const'
 
 import _ from 'lodash'
 
 
 class BillNew extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            'multiplePayments': false
-        }
-    }
-
     renderField(field) {
         return (
             <div>
@@ -26,33 +16,6 @@ class BillNew extends Component {
                     { ...field.input }
                 />
                 {field.meta.touched ? field.meta.error : ''}
-            </div>
-        )
-    }
-
-    monthsAndDays() {
-        return (
-            <div>
-                <div className="months-column">
-                    <h4 className="months-h4">Check Months To Pay Bill <span className="days-h4">Check Days to Pay Bill</span></h4>
-                    <div className="row">
-                        <div className="column column-20">
-                            <Field name="months" component={CheckboxGroup} options={MONTHS.slice(0,6)} />
-                        </div>
-                        <div className="column column-20">
-                            <Field name="months" component={CheckboxGroup} options={MONTHS.slice(6,12)} />
-                        </div>
-                        <div className="column column-20">
-                            <Field name="days" component={CheckboxGroup} options={DAYS.slice(0,10)} />
-                        </div>
-                        <div className="column column-20">
-                            <Field name="days" component={CheckboxGroup} options={DAYS.slice(10,20)} />
-                        </div>
-                        <div className="column column-20">
-                            <Field name="days" component={CheckboxGroup} options={DAYS.slice(20,31)} />
-                        </div>
-                    </div>
-                </div>
             </div>
         )
     }
@@ -95,23 +58,6 @@ class BillNew extends Component {
                     type="text"
                     component={this.renderField}
                 />
-                
-                <label htmlFor="multiplePaymentsCheckbox">
-                    <input 
-                        id="multiplePaymentsCheckbox" 
-                        type="checkbox" 
-                        onChange={() => {
-                            this.setState({'multiplePayments': !this.state.multiplePayments})
-                        }}
-                    />
-                    <span className="option-span">Create Multiple Payments</span>
-                </label>
-                {
-                    this.state.multiplePayments ? 
-                    this.monthsAndDays()
-                    :
-                    <div></div>
-                }
                 <button type="submit">Submit</button>
             </form>
         )
