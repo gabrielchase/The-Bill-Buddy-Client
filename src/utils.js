da95368df59d65796452872e2483dd05153b0969
gabrielchase <gchase.patron@gmail.com>
2018-02-12 20:13:56 +0800

    Remove everything from localStorage if passed the expiration date

--- src/utils.js
+++ src/utils.js
@@ -12,3 +12,17 @@ export const callAPI = () => {
     console.log('calling API')
     axios.get(ROOT_URL)
 }
+
+export const checkJWTExpiration = () => {
+    if (localStorage.getItem('jwt')) {
+        let now = new Date()
+        let expiration = new Date(localStorage.getItem('jwt_expiration') * 1000)
+        
+        if (now > expiration) {
+            localStorage.removeItem('user_id')
+            localStorage.removeItem('jwt')
+            localStorage.removeItem('jwt_creation')
+            localStorage.removeItem('jwt_expiration')
+        }
+    }
+}
